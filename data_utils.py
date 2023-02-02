e04ec87557cbda2abf6d279701563996c7ae19fe
innnky <xiyiji508@gmail.com>
2023-02-02 21:49:56 +0800

    win linux统一使用"/"

--- data_utils.py
+++ data_utils.py
@@ -54,7 +54,7 @@ class TextAudioSpeakerLoader(torch.utils.data.Dataset):
             spec = torch.squeeze(spec, 0)
             torch.save(spec, spec_filename)
 
-        spk = filename.split(os.sep)[-2]
+        spk = filename.split("/")[-2]
         spk = torch.LongTensor([self.spk_map[spk]])
 
         c = torch.load(filename + ".soft.pt").squeeze(0)
@@ -127,7 +127,7 @@ class EvalDataLoader(torch.utils.data.Dataset):
             spec = torch.squeeze(spec, 0)
             torch.save(spec, spec_filename)
 
-        spk = filename.split(os.sep)[-2]
+        spk = filename.split("/")[-2]
         spk = torch.LongTensor([self.spk_map[spk]])
 
         c = torch.load(filename + ".soft.pt").squeeze(0)
