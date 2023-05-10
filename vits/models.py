d68755018517689e90e66c0b906db53b44627ab9
MaxMax2016 <525942103@qq.com>
2023-05-10 21:15:51 +0800

    GRL

--- vits/models.py
+++ vits/models.py
@@ -8,6 +8,7 @@ from vits import commons
 from vits import modules
 from vits.utils import f0_to_coarse
 from vits_decoder.generator import Generator
+from vits.modules_grl import SpeakerClassifier
 
 
 class TextEncoder(nn.Module):
@@ -44,7 +45,7 @@ class TextEncoder(nn.Module):
         stats = self.proj(x) * x_mask
         m, logs = torch.split(stats, self.out_channels, dim=1)
         z = (m + torch.randn_like(m) * torch.exp(logs)) * x_mask
-        return z, m, logs, x_mask
+        return z, m, logs, x_mask, x
 
 
 class ResidualCouplingBlock(nn.Module):
@@ -172,7 +173,7 @@ class SynthesizerTrn(nn.Module):
 
     def forward(self, ppg, pit, spec, spk, ppg_l, spec_l):
         g = self.emb_g(F.normalize(spk)).unsqueeze(-1)
-        z_p, m_p, logs_p, ppg_mask = self.enc_p(
+        z_p, m_p, logs_p, ppg_mask, x = self.enc_p(
             ppg, ppg_l, f0=f0_to_coarse(pit))
         z_q, m_q, logs_q, spec_mask = self.enc_q(spec, spec_l, g=g)
 
@@ -186,11 +187,83 @@ class SynthesizerTrn(nn.Module):
         return audio, ids_slice, spec_mask, (z_f, z_r, z_p, m_p, logs_p, z_q, m_q, logs_q, logdet_f, logdet_r)
 
     def infer(self, ppg, pit, spk, ppg_l):
-        z_p, m_p, logs_p, ppg_mask = self.enc_p(
+        z_p, m_p, logs_p, ppg_mask, x = self.enc_p(
             ppg, ppg_l, f0=f0_to_coarse(pit))
         z, _ = self.flow(z_p, ppg_mask, g=spk, reverse=True)
         o = self.dec(spk, z * ppg_mask, f0=pit)
         return o
+
+
+class SynthesizerTrnEx(nn.Module):
+    def __init__(
+        self,
+        spec_channels,
+        segment_size,
+        hp
+    ):
+        super().__init__()
+        self.segment_size = segment_size
+        self.emb_g = nn.Linear(hp.vits.spk_dim, hp.vits.gin_channels)
+        self.enc_p = TextEncoder(
+            hp.vits.ppg_dim,
+            hp.vits.inter_channels,
+            hp.vits.hidden_channels,
+            hp.vits.filter_channels,
+            2,
+            6,
+            3,
+            0.1,
+        )
+        self.speaker_classifier = SpeakerClassifier(
+            hp.vits.hidden_channels,
+            hp.vits.spk_dim,
+        )
+        self.enc_q = PosteriorEncoder(
+            spec_channels,
+            hp.vits.inter_channels,
+            hp.vits.hidden_channels,
+            5,
+            1,
+            16,
+            gin_channels=hp.vits.gin_channels,
+        )
+        self.flow = ResidualCouplingBlock(
+            hp.vits.inter_channels,
+            hp.vits.hidden_channels,
+            5,
+            1,
+            4,
+            gin_channels=hp.vits.spk_dim
+        )
+        self.dec = Generator(hp=hp)
+
+    def forward(self, ppg, pit, spec, spk, ppg_l, spec_l):
+        g = self.emb_g(F.normalize(spk)).unsqueeze(-1)
+        z_p, m_p, logs_p, ppg_mask, x = self.enc_p(
+            ppg, ppg_l, f0=f0_to_coarse(pit))
+        z_q, m_q, logs_q, spec_mask = self.enc_q(spec, spec_l, g=g)
+
+        z_slice, pit_slice, ids_slice = commons.rand_slice_segments_with_pitch(
+            z_q, pit, spec_l, self.segment_size)
+        audio = self.dec(spk, z_slice, pit_slice)
+
+        # SNAC to flow
+        z_f, logdet_f = self.flow(z_q, spec_mask, g=spk)
+        z_r, logdet_r = self.flow(z_p, spec_mask, g=spk, reverse=True)
+        # speaker
+        spk_preds = self.speaker_classifier(x)
+        return audio, ids_slice, spec_mask, (z_f, z_r, z_p, m_p, logs_p, z_q, m_q, logs_q, logdet_f, logdet_r)
+
+    def infer(self, ppg, pit, spk, ppg_l):
+        z_p, m_p, logs_p, ppg_mask, x = self.enc_p(
+            ppg, ppg_l, f0=f0_to_coarse(pit))
+        z, _ = self.flow(z_p, ppg_mask, g=spk, reverse=True)
+        o = self.dec(spk, z * ppg_mask, f0=pit)
+        return o
+
+    def enhance_train(self):
+        for p in self.enc_q.parameters():
+           p.requires_grad = False
 
 
 class SynthesizerInfer(nn.Module):
@@ -233,7 +306,7 @@ class SynthesizerInfer(nn.Module):
         return self.dec.source2wav(source)
 
     def inference(self, ppg, pit, spk, ppg_l, source):
-        z_p, m_p, logs_p, ppg_mask = self.enc_p(
+        z_p, m_p, logs_p, ppg_mask, x = self.enc_p(
             ppg, ppg_l, f0=f0_to_coarse(pit))
         z, _ = self.flow(z_p, ppg_mask, g=spk, reverse=True)
         o = self.dec.inference(spk, z * ppg_mask, source)
--- vits/modules_grl.py
+++ vits/modules_grl.py
@@ -1,4 +1,3 @@
-# Adapted from https://github.com/keonlee9420/Daft-Exprt MIT License
 # Adapted from https://github.com/ubisoft/ubisoft-laforge-daft-exprt Apache License Version 2.0
 # Unsupervised Domain Adaptation by Backpropagation
 
@@ -32,9 +31,9 @@ class GradientReversal(torch.nn.Module):
         In the backward pass, upstream gradients are multiplied by -lambda (i.e. gradient are reversed)
     '''
 
-    def __init__(self, hparams):
+    def __init__(self, lambda_reversal=1):
         super(GradientReversal, self).__init__()
-        self.lambda_ = hparams.lambda_reversal
+        self.lambda_ = lambda_reversal
 
     def forward(self, x):
         return GradientReversalFunction.apply(x, self.lambda_)
@@ -60,22 +59,20 @@ class LinearNorm(nn.Module):
         return x
 
 
-class SpeakerRClassifier(nn.Module):
+class SpeakerClassifier(nn.Module):
     ''' Speaker Classifier Module:
         - 3x Linear Layers with ReLU
     '''
 
-    def __init__(self, hparams):
-        super(SpeakerRClassifier, self).__init__()
-        nb_speakers = hparams.n_speakers - 1
-        embed_dim = hparams.prosody_encoder['hidden_embed_dim']
+    def __init__(self, embed_dim, spk_dim):
+        super(SpeakerClassifier, self).__init__()
         self.classifier = nn.Sequential(
-            GradientReversal(hparams),
+            GradientReversal(lambda_reversal=1),
             LinearNorm(embed_dim, embed_dim, w_init_gain='relu'),
             nn.ReLU(),
             LinearNorm(embed_dim, embed_dim, w_init_gain='relu'),
             nn.ReLU(),
-            LinearNorm(embed_dim, nb_speakers, w_init_gain='linear')
+            LinearNorm(embed_dim, spk_dim, w_init_gain='linear')
         )
 
     def forward(self, x):
@@ -87,156 +84,9 @@ class SpeakerRClassifier(nn.Module):
         return outputs
 
 
-class GradientReversalLayer(nn.Module):
-    def __init__(self, alpha=1):
-        """
-        A gradient reversal layer.
-        This layer has no parameters, and simply reverses the gradient
-        in the backward pass.
-        """
-        super().__init__()
-        self._alpha = torch.tensor(alpha, requires_grad=False)
-
-    def forward(self, input_):
-        return RevGrad.apply(input_, self._alpha)
-
-
-class RevGrad(torch.autograd.Function):
-    """
-    A gradient reversal layer.
-    This layer has no parameters, and simply reverses the gradient in the backward pass.
-    See https://www.codetd.com/en/article/11984164, https://github.com/janfreyberg/pytorch-revgrad
-    """
-    @staticmethod
-    def forward(ctx, input_, alpha_):
-        ctx.save_for_backward(input_, alpha_)
-        output = input_
-        return output
-
-    @staticmethod
-    def backward(ctx, grad_output):  # pragma: no cover
-        grad_input = None
-        _, alpha_ = ctx.saved_tensors
-        if ctx.needs_input_grad[0]:
-            grad_input = -grad_output * alpha_
-        return grad_input, None
-
-
-class FiLM(nn.Module):
-    """
-    A Feature-wise Linear Modulation Layer from
-    'FiLM: Visual Reasoning with a General Conditioning Layer'
-    , extended to 'TADAM: Task dependent adaptive metric for improved few-shot learning'
-    """
-
-    def __init__(self):
-        super(FiLM, self).__init__()
-        self.s_gamma = nn.Parameter(torch.ones(1,), requires_grad=True)
-        self.s_beta = nn.Parameter(torch.ones(1,), requires_grad=True)
-
-    def forward(self, x, gammas, betas):
-        """
-        x -- [B, T, H]
-        gammas -- [B, 1, H]
-        betas -- [B, 1, H]
-        """
-        gammas = self.s_gamma * gammas.expand_as(x)
-        betas = self.s_beta * betas.expand_as(x)
-        return (gammas + 1.0) * x + betas
-
-
-class SpeakerClassifier(nn.Module):
-    """ Speaker Classifier """
-
-    def __init__(self, model_config):
-        super(SpeakerClassifier, self).__init__()
-        n_speaker = model_config["n_speaker"]
-        input_dim = model_config["prosody_encoder"]["encoder_hidden"]
-        self.hidden = model_config["prosody_encoder"]["encoder_hidden"]
-        self.grl = GradientReversalLayer()
-        self.classifier = nn.Sequential(OrderedDict([
-            ('fc1', nn.Linear(input_dim, self.hidden)),
-            ('ln1', nn.LayerNorm(self.hidden)),
-            ('relu1', nn.ReLU()),
-            ('fc2', nn.Linear(self.hidden, self.hidden)),
-            ('ln2', nn.LayerNorm(self.hidden)),
-            ('relu2', nn.ReLU()),
-            ('fc3', nn.Linear(self.hidden, n_speaker)),
-            ('softmax', nn.LogSoftmax(dim=-1))
-        ]))
-
-    def forward(self, x):
-        # GRL
-        rev_x = self.grl(x)
-        # Calculate augmentation posterior
-        score = self.classifier(rev_x)
-        if len(score.size()) > 2:
-            score = score.mean(dim=1)
-        return score  # [batch, 2]
-
 
 # -- Speaker Classifier
 # speaker_posterior = self.speaker_classifier(prosody_vector.squeeze(1))
 # LOSS
 # self.nll_loss = nn.NLLLoss()
 # adv_loss = self.nll_loss(speaker_posteriors, speaker_targets[序号])
-
-# pylint: disable=W0223
-# class GradientReversalFunction(torch.autograd.Function):
-#     """Revert gradient without any further input modification.
-#     Adapted from: https://github.com/Tomiinek/Multilingual_Text_to_Speech/"""
-# 
-#     @staticmethod
-#     def forward(ctx, x, l, c):
-#         ctx.l = l
-#         ctx.c = c
-#         return x.view_as(x)
-# 
-#     @staticmethod
-#     def backward(ctx, grad_output):
-#         grad_output = grad_output.clamp(-ctx.c, ctx.c)
-#         return ctx.l * grad_output.neg(), None, None
-# 
-# 
-# class ReversalClassifier(nn.Module):
-#     """Adversarial classifier with an optional gradient reversal layer.
-#     Adapted from: https://github.com/Tomiinek/Multilingual_Text_to_Speech/
-#     Args:
-#         in_channels (int): Number of input tensor channels.
-#         out_channels (int): Number of output tensor channels (Number of classes).
-#         hidden_channels (int): Number of hidden channels.
-#         gradient_clipping_bound (float): Maximal value of the gradient which flows from this module. Default: 0.25
-#         scale_factor (float): Scale multiplier of the reversed gradientts. Default: 1.0
-#         reversal (bool): If True reversal the gradients. Default: True
-#     """
-# 
-#     def __init__(
-#         self, in_channels, out_channels, hidden_channels, gradient_clipping_bounds=0.25, scale_factor=1.0, reversal=True
-#     ):
-#         super().__init__()
-#         self.reversal = reversal
-#         self._lambda = scale_factor
-#         self._clipping = gradient_clipping_bounds
-#         self._out_channels = out_channels
-#         self._classifier = nn.Sequential(
-#             nn.Linear(in_channels, hidden_channels), nn.ReLU(), nn.Linear(hidden_channels, out_channels)
-#         )
-# 
-#     def forward(self, x, labels, x_mask=None):
-#         if self.reversal:
-#             x = GradientReversalFunction.apply(x, self._lambda, self._clipping)
-#         x = self._classifier(x)
-#         loss = self.loss(labels, x, x_mask)
-#         return x, loss
-# 
-#     @staticmethod
-#     def loss(labels, predictions, x_mask=None, ignore_index=-100):
-#         if x_mask is None:
-#             x_mask = torch.Tensor([predictions.size(1)]).repeat(predictions.size(0)).int().to(predictions.device)
-#             ml = torch.max(x_mask)
-#             input_mask = torch.arange(ml, device=predictions.device)[None, :] < x_mask[:, None]
-#         else:
-#             input_mask = x_mask.squeeze().bool()
-#         target = labels.repeat(input_mask.size(-1), 1).transpose(0, 1).int().long()
-#         target[~input_mask] = ignore_index
-#         return nn.functional.cross_entropy(predictions.transpose(1, 2), target, ignore_index=ignore_index)
