87c324e0f553aeeb4e73d84ad55a8ccdb6c987af
MaxFish <525942103@qq.com>
2023-05-16 09:51:24 +0800

    nsf merge

--- vits_decoder/nsf.py
+++ vits_decoder/nsf.py
@@ -356,34 +356,15 @@ class SourceModuleCycNoise_v1(torch.nn.Module):
 
 
 class SourceModuleHnNSF(torch.nn.Module):
-    """SourceModule for hn-nsf
-    SourceModule(sampling_rate, harmonic_num=0, sine_amp=0.1,
-                 add_noise_std=0.003, voiced_threshod=0)
-    sampling_rate: sampling_rate in Hz
-    harmonic_num: number of harmonic above F0 (default: 0)
-    sine_amp: amplitude of sine source signal (default: 0.1)
-    add_noise_std: std of additive Gaussian noise (default: 0.003)
-        note that amplitude of noise in unvoiced is decided
-        by sine_amp
-    voiced_threshold: threhold to set U/V given F0 (default: 0)
-
-    Sine_source, noise_source = SourceModuleHnNSF(F0_sampled)
-    F0_sampled (batchsize, length, 1)
-    Sine_source (batchsize, length, 1)
-    noise_source (batchsize, length 1)
-    uv (batchsize, length, 1)
-    """
-
     def __init__(
         self,
         sampling_rate=32000,
-        harmonic_num=8,
         sine_amp=0.1,
         add_noise_std=0.003,
         voiced_threshod=0,
     ):
         super(SourceModuleHnNSF, self).__init__()
-
+        harmonic_num = 8
         self.sine_amp = sine_amp
         self.noise_std = add_noise_std
 
@@ -393,17 +374,21 @@ class SourceModuleHnNSF(torch.nn.Module):
         )
 
         # to merge source harmonics into a single excitation
-        self.l_linear = torch.nn.Linear(harmonic_num + 1, 1)
         self.l_tanh = torch.nn.Tanh()
+        self.register_buffer('merge_w', torch.FloatTensor([[
+            -0.1044, -0.4892, -0.4733, 0.4337, -0.2321,
+            -0.1889, 0.1315, -0.1002, 0.0590,]]))
+        self.register_buffer('merge_b', torch.FloatTensor([-0.2908]))
 
     def forward(self, x):
         """
-        Sine_source, noise_source = SourceModuleHnNSF(F0_sampled)
+        Sine_source = SourceModuleHnNSF(F0_sampled)
         F0_sampled (batchsize, length, 1)
         Sine_source (batchsize, length, 1)
-        noise_source (batchsize, length 1)
         """
         # source for harmonic branch
         sine_wavs = self.l_sin_gen(x)
-        sine_merge = self.l_tanh(self.l_linear(sine_wavs))
+        sine_wavs = torch_nn_func.linear(
+            sine_wavs, self.merge_w) + self.merge_b
+        sine_merge = self.l_tanh(sine_wavs)
         return sine_merge
