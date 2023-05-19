761720fd12e3a6d4c8a50478eeb0bf1861572ed5
MaxFish <525942103@qq.com>
2023-05-19 13:50:06 +0800

    discriminator

--- vits_decoder/discriminator.py
+++ vits_decoder/discriminator.py
@@ -1,36 +1,10 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 from omegaconf import OmegaConf
-from torch.nn.utils import weight_norm
+
 from .mpd import MultiPeriodDiscriminator
 from .mrd import MultiResolutionDiscriminator
-
-
-class DiscriminatorS(torch.nn.Module):
-    def __init__(self):
-        super(DiscriminatorS, self).__init__()
-        self.convs = nn.ModuleList([
-            weight_norm(nn.Conv1d(1, 16, 15, 1, padding=7)),
-            weight_norm(nn.Conv1d(16, 64, 41, 4, groups=4, padding=20)),
-            weight_norm(nn.Conv1d(64, 256, 41, 4, groups=16, padding=20)),
-            weight_norm(nn.Conv1d(256, 1024, 41, 4, groups=64, padding=20)),
-            weight_norm(nn.Conv1d(1024, 1024, 41, 4, groups=256, padding=20)),
-            weight_norm(nn.Conv1d(1024, 1024, 5, 1, padding=2)),
-        ])
-        self.conv_post = weight_norm(nn.Conv1d(1024, 1, 3, 1, padding=1))
-
-    def forward(self, x):
-        fmap = []
-        for l in self.convs:
-            x = l(x)
-            x = F.leaky_relu(x, 0.1)
-            fmap.append(x)
-        x = self.conv_post(x)
-        fmap.append(x)
-        x = torch.flatten(x, 1, -1)
-        return [(fmap, x)]
 
 
 class Discriminator(nn.Module):
@@ -39,22 +13,27 @@ class Discriminator(nn.Module):
         self.MRD = MultiResolutionDiscriminator(hp)
         self.MPD = MultiPeriodDiscriminator(hp)
 
+
     def forward(self, x):
-        return self.MRD(x), self.MPD(x)
+        r = self.MRD(x)
+        p = self.MPD(x)
+
+        return r + p
 
 
 if __name__ == '__main__':
-    hp = OmegaConf.load('../config/default.yaml')
+    hp = OmegaConf.load('../config/base.yaml')
     model = Discriminator(hp)
 
     x = torch.randn(3, 1, 16384)
     print(x.shape)
 
-    mrd_output, mpd_output = model(x)
-    for features, score in mpd_output:
+    output = model(x)
+    for features, score in output:
         for feat in features:
             print(feat.shape)
         print(score.shape)
 
-    pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    pytorch_total_params = sum(p.numel()
+                               for p in model.parameters() if p.requires_grad)
     print(pytorch_total_params)
