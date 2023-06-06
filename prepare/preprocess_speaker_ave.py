576d12f2feb031ef4c1516b162bf10aa513917fc
TheMandateOfRock <38366253+OOPPEENN@users.noreply.github.com>
2023-06-06 10:56:56 +0800

    Update preprocess_speaker_ave.py

--- prepare/preprocess_speaker_ave.py
+++ prepare/preprocess_speaker_ave.py
@@ -1,4 +1,8 @@
 import os
+import sys
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+sys.path.append(parent_dir)
 import argparse
 import numpy as np
 
