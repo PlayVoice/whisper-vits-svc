fe0ce0c732b05ec776ac4fa3840398ab431def00
TheMandateOfRock <38366253+OOPPEENN@users.noreply.github.com>
2023-06-18 18:30:15 +0800

    Update svc_preprocessing.py
    speaker
    speaker_ave
    spec
    train
    这四步不怎么占用资源，默认使用全部CPU核心

--- svc_preprocessing.py
+++ svc_preprocessing.py
@@ -13,10 +13,10 @@ if args.crepe:
       "python prepare/preprocess_a.py -w ./dataset_raw -o ./data_svc/waves-32k -s 32000 -t 0",
       "python prepare/preprocess_f0_crepe.py -w data_svc/waves-16k/ -p data_svc/pitch -t "+str(args.t),
       "python prepare/preprocess_ppg.py -w data_svc/waves-16k/ -p data_svc/whisper -t 1",
-      "python prepare/preprocess_speaker.py data_svc/waves-16k/ data_svc/speaker -t "+str(args.t),
-      "python prepare/preprocess_speaker_ave.py data_svc/speaker/ data_svc/singer -t "+str(args.t),
-      "python prepare/preprocess_spec.py -w data_svc/waves-32k/ -s data_svc/specs -t "+str(args.t),
-      "python prepare/preprocess_train.py -t "+str(args.t),
+      "python prepare/preprocess_speaker.py data_svc/waves-16k/ data_svc/speaker -t 0",
+      "python prepare/preprocess_speaker_ave.py data_svc/speaker/ data_svc/singer -t 0",
+      "python prepare/preprocess_spec.py -w data_svc/waves-32k/ -s data_svc/specs -t 0",
+      "python prepare/preprocess_train.py -t 0",
       "python prepare/preprocess_zzz.py",
    ]
 
@@ -26,10 +26,10 @@ else:
       "python prepare/preprocess_a.py -w ./dataset_raw -o ./data_svc/waves-32k -s 32000 -t 0",
       "python prepare/preprocess_f0.py -w data_svc/waves-16k/ -p data_svc/pitch -t "+str(args.t),
       "python prepare/preprocess_ppg.py -w data_svc/waves-16k/ -p data_svc/whisper -t 1",
-      "python prepare/preprocess_speaker.py data_svc/waves-16k/ data_svc/speaker -t "+str(args.t),
-      "python prepare/preprocess_speaker_ave.py data_svc/speaker/ data_svc/singer -t "+str(args.t),
-      "python prepare/preprocess_spec.py -w data_svc/waves-32k/ -s data_svc/specs -t "+str(args.t),
-      "python prepare/preprocess_train.py -t "+str(args.t),
+      "python prepare/preprocess_speaker.py data_svc/waves-16k/ data_svc/speaker -t 0",
+      "python prepare/preprocess_speaker_ave.py data_svc/speaker/ data_svc/singer -t 0",
+      "python prepare/preprocess_spec.py -w data_svc/waves-32k/ -s data_svc/specs -t 0",
+      "python prepare/preprocess_train.py -t 0",
       "python prepare/preprocess_zzz.py",
    ]
 
