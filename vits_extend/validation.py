36702fcf4247eccb208bbc4dcd91e194b6c0930a
MaxFish <525942103@qq.com>
2023-06-27 12:00:20 +0800

    valid hubert

--- vits_extend/validation.py
+++ vits_extend/validation.py
@@ -10,18 +10,19 @@ def validate(hp, args, generator, discriminator, valloader, stft, writer, step, 
 
     loader = tqdm.tqdm(valloader, desc='Validation loop')
     mel_loss = 0.0
-    for idx, (ppg, ppg_l, pit, spk, spec, spec_l, audio, audio_l) in enumerate(loader):
+    for idx, (ppg, ppg_l, vec, pit, spk, spec, spec_l, audio, audio_l) in enumerate(loader):
         ppg = ppg.to(device)
+        vec = vec.to(device)
         pit = pit.to(device)
         spk = spk.to(device)
         ppg_l = ppg_l.to(device)
         audio = audio.to(device)
 
         if hasattr(generator, 'module'):
-            fake_audio = generator.module.infer(ppg, pit, spk, ppg_l)[
+            fake_audio = generator.module.infer(ppg, vec, pit, spk, ppg_l)[
                 :, :, :audio.size(2)]
         else:
-            fake_audio = generator.infer(ppg, pit, spk, ppg_l)[
+            fake_audio = generator.infer(ppg, vec, pit, spk, ppg_l)[
                 :, :, :audio.size(2)]
 
         mel_fake = stft.mel_spectrogram(fake_audio.squeeze(1))
