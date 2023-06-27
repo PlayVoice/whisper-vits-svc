2508213a8f9e43d86c65dd8408bd71035845aa39
MaxFish <525942103@qq.com>
2023-06-27 11:54:04 +0800

    hubert

--- svc_preprocessing.py
+++ svc_preprocessing.py
@@ -13,10 +13,11 @@ if args.crepe:
       "python prepare/preprocess_a.py -w ./dataset_raw -o ./data_svc/waves-32k -s 32000 -t 0",
       "python prepare/preprocess_f0_crepe.py -w data_svc/waves-16k/ -p data_svc/pitch -t "+str(args.t),
       "python prepare/preprocess_ppg.py -w data_svc/waves-16k/ -p data_svc/whisper -t 1",
+      "python prepare/preprocess_hubert.py -w data_svc/waves-16k/ -v data_svc/hubert -t 1",
       "python prepare/preprocess_speaker.py data_svc/waves-16k/ data_svc/speaker -t 0",
       "python prepare/preprocess_speaker_ave.py data_svc/speaker/ data_svc/singer -t 0",
       "python prepare/preprocess_spec.py -w data_svc/waves-32k/ -s data_svc/specs -t 0",
-      "python prepare/preprocess_train.py -t 0",
+      "python prepare/preprocess_train.py",
       "python prepare/preprocess_zzz.py",
    ]
 
@@ -26,10 +27,11 @@ else:
       "python prepare/preprocess_a.py -w ./dataset_raw -o ./data_svc/waves-32k -s 32000 -t 0",
       "python prepare/preprocess_f0.py -w data_svc/waves-16k/ -p data_svc/pitch -t "+str(args.t),
       "python prepare/preprocess_ppg.py -w data_svc/waves-16k/ -p data_svc/whisper -t 1",
+      "python prepare/preprocess_hubert.py -w data_svc/waves-16k/ -v data_svc/hubert -t 1",
       "python prepare/preprocess_speaker.py data_svc/waves-16k/ data_svc/speaker -t 0",
       "python prepare/preprocess_speaker_ave.py data_svc/speaker/ data_svc/singer -t 0",
       "python prepare/preprocess_spec.py -w data_svc/waves-32k/ -s data_svc/specs -t 0",
-      "python prepare/preprocess_train.py -t 0",
+      "python prepare/preprocess_train.py",
       "python prepare/preprocess_zzz.py",
    ]
 
