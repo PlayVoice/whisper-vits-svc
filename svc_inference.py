20fb3f50695592717d9f58b3558e1e247c276eae
MaxMax2016 <525942103@qq.com>
2023-07-10 22:26:30 +0800

    path

--- svc_inference.py
+++ svc_inference.py
@@ -1,5 +1,5 @@
 import sys,os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import torch
 import argparse
 import numpy as np
