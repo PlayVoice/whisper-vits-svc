d40e46a93c84eccad9708405388614eb9bb581f4
MaxFish <525942103@qq.com>
2023-08-04 08:08:54 +0800

    Update preprocess_crepe.py

--- prepare/preprocess_crepe.py
+++ prepare/preprocess_crepe.py
@@ -73,7 +73,7 @@ if __name__ == "__main__":
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     if device == "cuda":
-      set_start_method('spawn')
+      set_start_method('spawn', force=True)
 
     for spks in os.listdir(wavPath):
         if os.path.isdir(f"./{wavPath}/{spks}"):
