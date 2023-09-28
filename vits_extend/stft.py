4568740327fc229dc2a4c6d0951bfffedfb639a3
MaxFish <16432329+MaxMax2016@users.noreply.github.com>
2023-09-28 10:30:12 +0800

    HiFTNet

--- vits_extend/stft.py
+++ vits_extend/stft.py
@@ -78,8 +78,8 @@ class TacotronSTFT(torch.nn.Module):
         -------
         mel_output: torch.FloatTensor of shape (B, n_mel_channels, T)
         """
-        assert(torch.min(y.data) >= -1)
-        assert(torch.max(y.data) <= 1)
+        # assert(torch.min(y.data) >= -1)
+        # assert(torch.max(y.data) <= 1)
 
         y = torch.nn.functional.pad(y.unsqueeze(1),
                                     (int((self.n_fft - self.hop_size) / 2), int((self.n_fft - self.hop_size) / 2)),
